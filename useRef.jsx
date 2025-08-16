0fd10a7c59c77434bc9353234676f5e05e1b7f70
RtKush <164990467+RtKush@users.noreply.github.com>
2025-08-16 08:50:40 +0530

    Create useRef.jsx

---
+++ useRef.jsx
@@ -0,0 +1,14 @@
+import { useRef } from "react";
+import ReactDOM from "react-dom/client";
+function App() {
+const inputElement = useRef();
+const focusInput = () => {
+inputElement.current.focus();
+};
+return (
+<>
+<input type="text" ref={inputElement} />
+<button onClick={focusInput}>Focus Input</button>
+</>
+);
+}
