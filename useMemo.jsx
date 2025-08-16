1a3fc2231129eb70d76e85264e7f77a230c50348
RtKush <164990467+RtKush@users.noreply.github.com>
2025-08-16 08:48:16 +0530

    Create useMemo.jsx

---
+++ useMemo.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useMemo } from "react";
+function CounterMemo() {
+const [counteOne, setCounterOne] = useState(0);
+const [counteTwo, setCounterTwo] = useState(0);
+const incrementOne = () => {
+setCounterOne(counteOne + 1);
+};
+const incrementTwo = () => {
+setCounterTwo(counteTwo + 1);
+};
+const isEven = useMemo(() => {
+let i = 0;
+let j = 0;
+while (i < 9000 && j < 9000022) i = j++;
+return counteOne % 2 === 0;
+}, [counteOne]);
+return (
+<div>
+<button onClick={incrementOne}>Increment one{counteOne}</button>
+<br />
+<span>{isEven ? "even" : "odd"}</span>
+<br />
+<button onClick={incrementTwo}>Increment two{counteTwo}</button>
+</div>
+);
+}
+export default CounterMemo;
